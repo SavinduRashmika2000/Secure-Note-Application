3d8ca3bb3933966d00b5374f894c433b9d409b7c
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-08-05 12:40:15 +0530

    create the model,controller,service,repo and connect to mysql database

---
+++ noteApp/src/main/java/com/secure/noteApp/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.secure.noteApp.repositories;
+
+import com.secure.noteApp.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByOwnerUsername(String ownerUsername);
+}
---
+++ noteApp/src/main/java/com/secure/noteApp/services/NoteService.java
@@ -0,0 +1,15 @@
+package com.secure.noteApp.services;
+
+import com.secure.noteApp.models.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    Note createNoteForUser(String username, String content);
+
+    Note updateNoteForUser(Long noteId, String content, String username);
+
+    void deleteNoteForUser(Long noteId, String username);
+
+    List<Note> getNotesForUser(String username);
+}
