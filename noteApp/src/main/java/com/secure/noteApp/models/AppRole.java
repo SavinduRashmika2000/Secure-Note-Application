cef6ff61d29b7b0bc2e9c911b1861842a2642c11
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-08-21 10:48:53 +0530

    create custom user model

---
+++ noteApp/src/main/java/com/secure/noteApp/models/AppRole.java
@@ -0,0 +1,10 @@
+package com.secure.noteApp.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
+
+
+
+
---
+++ noteApp/src/main/java/com/secure/noteApp/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.secure.noteApp.repositories;
+
+
+import com.secure.noteApp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+
+    boolean existsByUserName(String username);
+}
+
