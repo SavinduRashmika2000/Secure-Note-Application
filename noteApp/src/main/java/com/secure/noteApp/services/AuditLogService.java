a09ddd66e41a7fc4603064a8216988a71a6a93a7
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-10-27 19:09:22 +0530

    create auditLog service and service implementation

---
+++ noteApp/src/main/java/com/secure/noteApp/services/AuditLogService.java
@@ -0,0 +1,18 @@
+package com.secure.noteApp.services;
+
+import com.secure.noteApp.models.AuditLog;
+import com.secure.noteApp.models.Note;
+
+import java.util.List;
+
+public interface AuditLogService {
+    void logNoteCreation(String username, Note note);
+
+    void logNoteUpdate(String username, Note note);
+
+    void logNoteDeletion(String username, Long noteId);
+
+    List<AuditLog> getAllAuditLogs();
+
+    List<AuditLog> getAuditLogsForNoteId(Long id);
+}
