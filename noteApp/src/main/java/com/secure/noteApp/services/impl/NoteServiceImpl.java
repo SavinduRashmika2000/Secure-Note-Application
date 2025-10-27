6e46f3ccc3a6a8e3415a15bf5a8c1b115cdaec39
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-10-27 19:22:36 +0530

    update note service implementation by adding auditlog

--- noteApp/src/main/java/com/secure/noteApp/services/impl/NoteServiceImpl.java
+++ noteApp/src/main/java/com/secure/noteApp/services/impl/NoteServiceImpl.java
@@ -14,12 +14,17 @@ public class NoteServiceImpl implements NoteService {
     @Autowired
     private NoteRepository noteRepository;
 
+    @Autowired
+    private AuditLogServiceImpl auditLogService;
+
     @Override
     public Note createNoteForUser(String username, String content) {
         Note note = new Note();
         note.setContent(content);
         note.setOwnerUsername(username);
-        return noteRepository.save(note);
+        Note savedNote = noteRepository.save(note);
+        auditLogService.logNoteCreation(username, note);
+        return savedNote;
     }
 
     @Override
@@ -27,12 +32,18 @@ public class NoteServiceImpl implements NoteService {
         Note note = noteRepository.findById(noteId).orElseThrow(()
                 -> new RuntimeException("Note not found"));
         note.setContent(content);
-        return noteRepository.save(note);
+        Note updatedNote = noteRepository.save(note);
+        auditLogService.logNoteUpdate(username, note);
+        return updatedNote;
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
-        noteRepository.deleteById(noteId);
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                () -> new RuntimeException("Note not found")
+        );
+        auditLogService.logNoteDeletion(username, noteId);
+        noteRepository.delete(note);
     }
 
     @Override
